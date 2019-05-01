65449e1dfab45baf0bab5195d0bfd13d406995af
Keiran Raine <kr2@sanger.ac.uk>
2019-05-01 13:00:20 +0000

    Update Java to OpenJDK

--- scripts/ubuntu/build.sh
+++ scripts/ubuntu/build.sh
@@ -16,11 +16,11 @@ sudo apt-get -y install docker-ce=$DOCKER_VERSION
 sudo usermod -aG docker ubuntu
 
 ## JAVA for DOCKSTORE - no jre8 in apt by default
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo bash -c '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections'
-sudo bash -c '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections'
+sudo add-apt-repository -y ppa:openjdk-r/ppa
+#sudo bash -c '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections'
+#sudo bash -c '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections'
 sudo apt-get -yq update
-sudo apt-get -yq install --no-install-recommends oracle-java8-installer
+sudo apt-get -yq install --no-install-recommends openjdk-8-jdk
 
 # useful tools and python deps
 sudo apt-get -yq install --no-install-recommends nano less python-dev python-pip
